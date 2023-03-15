419734d4cc17193c1ac44bddb5eb0011e8cd054b
then0403 <39440926+then0403@users.noreply.github.com>
2023-03-15 14:27:17 -0700

    first commit

--- components/Services.js
+++ components/Services.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+//import components
+import ServiceSlider from "./ServiceSlider";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="bg-services bg-cover bg-no-repeat bg-center min-h-[862px]  py-8">
+      <div className="container mx-auto pl-10 h-[862px]">
+        <h2 className="h2 mb-12">Our Services</h2>
+        <ServiceSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Services;
